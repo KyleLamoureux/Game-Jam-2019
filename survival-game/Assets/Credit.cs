953d77b4aed6a6739f566138d316317845293a7b
HakuFlaku <john.i.visser@gmail.com>
2019-11-10 13:15:37 -0600

    Adding credits and menu

---
+++ survival-game/Assets/Credit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credit : MonoBehaviour
+{
+    public bool isTitle;
+    public bool isQuit;
+    void OnMouseUp()
+    {
+        if (isTitle)
+        {
+            Application.LoadLevel(0);
+        }
+        if (isQuit)
+        {
+            Application.Quit();
+        }
+    }
+}
